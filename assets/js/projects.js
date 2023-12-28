ca356321f44e848911bb56aad09fc762cfd1fe8c
geonwoo park <gw5994@naver.com>
2023-12-29 03:11:48 +0900

    pdf add

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -27,7 +27,8 @@ let render_projects = (slug) => {
             // demo: false,
             technologies: ['Python'],
             description: "[한국정보과학회] Short-From 영상 시청 패턴 분석 및 영상 시청 여부 예측 연구",
-            categories: ['featured','webdev']
+            categories: ['featured','webdev'],
+            pdf: 'assets/images/Short-Form 논문1.pdf' // Add the PDF link here
         },
         {
             image: 'assets/images/fat.jpg',
